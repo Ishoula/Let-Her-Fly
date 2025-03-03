b21f7045189fa781d3e0c6fa452c68ad00f05379
I.Shoula <165474242+Ishoula@users.noreply.github.com>
2025-03-03 09:58:13 +0200

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,66 @@
+document.getElementById("form-create").addEventListener('submit', function(event){
+    event.preventDefault()
+    const isValid=formValidate()
+   if(isValid){
+    alert("You have successfully registered with InfoNest 🤗. You can now login")
+    window.location.href= "login.html"
+   }
+
+   
+})
+function formValidate(){
+    let isValid=true
+    const name=document.getElementById("name")
+    if(!name.value.trim(name)){
+        name.classList.add('invalid')
+        name.nextElementSibling.style.display="block"
+        isValid=false
+    }
+    else{
+        name.classList.remove('invalid')
+        name.nextElementSibling.style.display="none"
+    }
+    const email =document.getElementById("email")
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email.value.trim(email))){
+        email.classList.add("invalid")
+        email.nextElementSibling.style.display="block"
+        isValid=false
+    }
+    else{
+        email.classList.remove('invalid')
+        email.nextElementSibling.style.display="none"
+    }
+    const birthdate=document.getElementById('birth')
+    if(!birthdate.value.trim()){
+        birthdate.classList.add('invalid')
+        birthdate.nextElementSibling.style.display="block"
+        isValid=false
+    }
+    else{
+        birthdate.classList.remove('invalid')
+        birthdate.nextElementSibling.style.display="none"
+    }
+    const school=document.getElementById('school')
+    if(!school.value.trim()){
+        school.classList.add('invalid')
+        school.nextElementSibling.style.display="block"
+        isValid=false
+    }
+    else{
+        school.classList.remove('invalid')
+        school.nextElementSibling.style.display="none"
+    }
+
+    const pass=document.getElementById("pass")
+    if(!pass.value.trim(pass)){
+        pass.classList.add("invalid")
+        pass.nextElementSibling.style.display="block"
+        isValid=false
+    }
+    else{
+        pass.classList.remove("invalid")
+        pass.nextElementSibling.style.display="none"
+    }
+    return isValid
+}
